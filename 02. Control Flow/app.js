958d1f53ae52f8561eb2c6da1c777a92bf7ae3d5
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-24 21:44:46 +0545

    learned about conditional statement

---
+++ 02. Control Flow/app.js
@@ -0,0 +1,110 @@
+//conditional Statement
+
+//psuedo code
+//if (condition) {... }
+//else if(condition){...}
+//else{...}
+
+// let a = 20;
+// let b = 20;
+// if (a > 20) {
+//   console.log("a is greater than b");
+// }
+// else if (a < b) {
+//   console.log("b is greater than a")
+// }
+// else {
+//   console.log("a is equal to b")
+// }
+
+// let time = 10;
+// let greetings;
+// if (time < 10) {
+//   greetings = "Good Morning";
+//   console.log(greetings)
+// }
+// else if (time < 20) {
+//   greetings = "Good Day";
+//   console.log(greetings)
+// }
+// else {
+//   greetings = "Good Evening"
+//   console.log(greetings)
+// }
+
+//challenges
+// let password;
+// if (password == 8) {
+//   console.log("Welcome")
+// }
+// else if (password < 8) {
+//   console.log("Password is short")
+// }
+// else if (password > 8) {
+//   console.log("Too long password");
+//   console.log("Password should be 8 characters");
+// }
+// else {
+//   console.log("Please Provide a password")
+// }
+
+//switch statement
+//pseudo code
+//switch(expression){
+//   case x:
+// //print
+// //break
+//     case y:
+// //print y;
+// break:
+// default
+// //print z
+// //break;
+// }
+
+// let x;
+// let bulb;
+// switch (x) {
+//   case 0:
+//     bulb = off;
+//     break;
+//   case 1:
+//     bulb = true
+//     break;
+//   default:
+//     console.log("No Value");
+// }
+// console.log(bulb)
+
+// let day = "sunday"
+// switch (day) {
+//   case "monday":
+//     console.log("Today is monday")
+//     break;
+//   case "tuesday":
+//     console.log("Today is tuesday")
+//     break;
+//   case "wednesday":
+//     console.log("Today is wednesday");
+//     break;
+//   default:
+//     console.log("Don't know which day is today!")
+// }
+
+//Challenges
+// let fruit = "asdasd";
+// switch (fruit) {
+//   case "banana":
+//     console.log("Banana is good");
+//     break;
+//   case "orange":
+//     console.log("I am not a fan of orange");
+//     break;
+//   case "apple":
+//     console.log("How you like them apples?");
+//     break;
+//   default:
+//     console.log("I have never heard of that fruit.");
+// }
+
+//for loops
