2c3600ae228c2c74216411b7f159b79d413f97e7
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-26 12:53:40 +0545

    learned template literals

---
+++ 07. intermediate/app.js
@@ -0,0 +1,32 @@
+// // //template strings also known as template literals
+// // const firstName = "Ayush"
+// // const lastName = "Pakhrin"
+// // // const greet = setInterval(function () {
+// // //   console.log(`Welcome ${firstName}!`);
+// // // }, 2000);
+// // // setTimeout(function () {
+// // //   clearInterval(greet);
+// // //   console.log("Stopped Greeting")
+// // // }, 10000);
+// // // console.log(`My name is ${firstName} ${lastName}.`);
+// // // console.log(`${['one', 'two', 'three']}`)
+// // // console.log(`${2 + 2}`)
+// // function info() {
+// //   return `My name is Ayush Pakhrin`;
+// // }
+// // console.log(`${info()}`)
+
+
+// //challenges
+// console.log(`
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+// `)
+
+// const firstName = "Ayush";
+// const lastName = "Pakhrin";
+// console.log(`My name is ${firstName} ${lastName}.`);
+
+//Ar
