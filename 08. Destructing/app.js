cf56f990bdd84dcd00f77d6d2e1eb9364de6772a
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-26 19:05:49 +0545

    learned about basic array destructing

---
+++ 08. Destructing/app.js
@@ -0,0 +1,35 @@
+// const foo = ['one', 'two', 'three'];
+
+// const [one, two, three] = foo;
+
+// console.log(three)
+// console.log(one);
+// const colors = ['red', 'green', 'blue'];
+// const [A, B, C] = colors;
+// console.log(A);
+// console.log(B);
+
+// let a, b;
+// [a = 5, b = 1] = ['one', 'two', 'three'];
+// console.log(b);
+
+// function f() {
+//   return ["Ayush", "Pakhrin"];
+// }
+// let a, b;
+// [a, b] = f();
+// console.log(a,b)
+
+// function f() {
+//   return [1, 2, 3];
+// }
+// const [a, , b] = f();
+// console.log();
+
+//Assigning the rest of an array to a variable
+// const [a, ...b] = ['one', 'two', 'three'];
+// console.log(b)
+
+// const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+// const [color1, color2, color3, ...color4] = colors;
+// console.log(color1,color2,color3,color4);
