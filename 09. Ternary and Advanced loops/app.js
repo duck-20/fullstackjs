24fe002534c6cd6f2156d679d35e99670a370194
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-27 18:21:02 +0545

    learned ternary operator

---
+++ 09. Ternary and Advanced loops/app.js
@@ -0,0 +1,22 @@
+//ternary operator
+//condition?expifTrue:expifFalse;
+
+// let isTrue = true;
+// isTrue ? console.log(true):console.log(false)
+
+// let password = 8;
+// function passwordChecker(ps) {
+//   return ps === 8 ? "Strong Password" : "Password should be 8 Character";
+// }
+// let res = passwordChecker(password);
+// console.log(res)
+
+// const age = 15
+
+// const isAdult = age >= 18 ? "Adult" : "Not an Adult";
+// console.log(isAdult)
+
+//challenges;
+let haveMoney = true;
+let res = haveMoney ? "Buy Products" : "They should bring money";
+console.log(res)
