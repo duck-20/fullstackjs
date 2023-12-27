fa02311acb04b595e39b65768129999a89daa224
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-27 20:28:16 +0545

    learned about reduce

--- 09. Ternary and Advanced loops/app.js
+++ 09. Ternary and Advanced loops/app.js
@@ -145,12 +145,119 @@
 // ];
 // console.log(words.filter(word => word.length > 6))
 
+//Find method
+
+// const peoples = [
+//   { name: "Ayush", age: 21 },
+//   { name: "Alex", age: 19 },
+//   { name: "huxn", age: 22 },
+//   { name: "jimy", age: 16 },
+//   { name: "Alex", age: 29 },
+// ]
+// const res = peoples.find(people => people.name = "alex");
+// console.log(res)
+
+// const posts = [
+//   { id: 1, content: "Good Post" },
+//   { id: 2, content: "Funny Post" },
+//   { id: 3, content: "Sad Post" },
+//   { id: 4, content: "Bad Post" },
+// ]
+// const res = posts.find(post => post.content == "Funny Post");
+// console.log(res);
+
+//challenges
+// const ages = [3, 10, 18, 20];
+// const res = ages.find(age => age > 18);
+// console.log(res)
 
 
+// let products = [
+//   { name: "Checkers", category: "toys" },
+//   { name: "Harry Potter", category: "Books" },
+//   { name: "iphone", category: "Electronics" },
+//   { name: "Learn PHP", category: "Books" },
+// ]
+// let res = products.find(product => product.category == "Books");
+// console.log(res)
+
+//every method and some method
+
+// const peoples = ['huxn', 'alex', 'ab'];
+// const res1=peoples.every(people => people.length==4);
+// // console.log(res1)
+// const res2 = peoples.some(people => people.length >3);
+// console.log(res2)
+
+// const songs = [
+//   { name: "Lucky You", duration: 4.32 },
+//   { name: "Just like you", duration: 3.23 },
+//   { name: "The search", duration: 2.32 },
+//   { name: "The Box", duration: 1.32 },
+// ]
+// const res = songs.some(song => song.duration > 3);
+// console.log(res);
+
+// let products = [
+//   { name: "Checkers", category: "toys" },
+//   { name: "Harry Potter", category: "Books" },
+//   { name: "iphone", category: "Electronics" },
+//   { name: "Learn PHP", category: "Books" },
+// ]
+// let everyBook = products.every(product => product.category === "Books");
+// let someBook = products.some(product => product.category === "Books");
+// console.log(everyBook);
+// console.log(someBook);
+
+//reduce method
+// let numbers = [1, 2, 3, 4, 5];
+// let res = numbers.reduce((p, c) => {
+//   console.log(`Pervious=${p}`)
+//   console.log(`Current=${c}`)
+//   return p + c
+// }, 0);
+
+// console.log(res)
 
 
+// let numbers = [5, 6, 7, 8, 9, 10];
+// const res = numbers.reduce((p, c) => p + c);
+// console.log(res);
 
 
+// let alpha = ['Abc', 'BCd', 'EFG'];
+// console.log(alpha.reduce((p, c) => p + c));
 
+// const peoples = [
+//   { name: "Ayush", age: 21 },
+//   { name: "Alex", age: 19 },
+//   { name: "huxn", age: 22 },
+//   { name: "jimy", age: 16 },
+//   { name: "Alex", age: 29 },
+// ]
+// const oldeestAge = peoples.reduce((p, c) => (c.age > p ? c.age : p), 0);
+// console.log(oldeestAge)
+// const words = [
+//   "Apple",
+//   "Banana",
+//   "Orange",
+//   "Banana",
+//   "Apple",
+//   "Apple",
+//   "Orange",
+//   "Grape"
+// ]
+// const wordFrequency = words.reduce((frequencyMap, word) => {
+//   frequencyMap[word] = (frequencyMap[word]) || 0 + 1;
+//   return frequencyMap;
+// }, {})
+// console.log(wordFrequency)
 
+//challenges
+// const numbers = [2, 3, 4, 5];
+// let calculateProduct = (arr) => arr.reduce((p, c) => p * c);
 
+// let product = calculateProduct(numbers);
+// console.log(product);
+
+//map
